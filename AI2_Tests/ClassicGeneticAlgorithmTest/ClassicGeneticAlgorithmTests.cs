ca473c91a845449bf119d82781fe17b3ae2e7850
Grivendel <tomasz.neter@mines.pl>
2022-11-16 20:16:15 +0100

    test ClassicGeneticAlgorithm_FitnessLargeThan5

--- AI2_Tests/ClassicGeneticAlgorithmTest/ClassicGeneticAlgorithmTests.cs
+++ AI2_Tests/ClassicGeneticAlgorithmTest/ClassicGeneticAlgorithmTests.cs
@@ -1,13 +1,17 @@
-﻿namespace AI2_UnitTests.ClassicGeneticAlgorithmTest {
+﻿namespace AI2_UnitTests.ClassicGeneticAlgorithmTest
+{
     [TestClass]
-    public class UnitTest1 {
+    public class UnitTest1
+    {
         [TestMethod]
-        public void ClassicGeneticAlgorithm_MinFitnessOtherThanZero() {
+        public void ClassicGeneticAlgorithm_MinFitnessOtherThanZero()
+        {
             //Arrange
-            ClassicGeneticAlgorithm classic = new(new BinaryMutation(0.05f), new RouletteWheelParentSelector(0.5f), new FIFOPopulationSelector(), new SinglePointCrossover(0.5f), 10);
+            ClassicGeneticAlgorithm classic = new(new BinaryMutation(0.05f), new RouletteWheelParentSelector(0.05f), new FIFOPopulationSelector(), new SinglePointCrossover(0.05f), 10);
 
             //Act
-            for (int i = 0; i < 100; i++) {
+            for (int i = 0; i < 100; i++)
+            {
                 classic.Update();
                 Console.WriteLine($"Iter: {(i + 1):00}\t{classic}");
             }
@@ -18,12 +22,14 @@
         }
 
         [TestMethod]
-        public void ClassicGeneticAlgorithm_MaxFitnessOptimal() {
+        public void ClassicGeneticAlgorithm_MaxFitnessReached()
+        {
             //Arrange
-            ClassicGeneticAlgorithm classic = new(new BinaryMutation(0.05f), new RouletteWheelParentSelector(0.5f), new FIFOPopulationSelector(), new SinglePointCrossover(0.5f), 10);
+            ClassicGeneticAlgorithm classic = new(new BinaryMutation(0.05f), new RouletteWheelParentSelector(0.05f), new FIFOPopulationSelector(), new SinglePointCrossover(0.05f), 10);
 
             //Act
-            for (int i = 0; i < 100; i++) {
+            for (int i = 0; i < 100; i++)
+            {
                 classic.Update();
                 Console.WriteLine($"Iter: {(i + 1):00}\t{classic}");
             }
@@ -31,6 +37,24 @@
             //Assert
             classic.GetAveragePopulationFitness(out _, out var maxFitness);
             Assert.AreEqual(5.6f, maxFitness);
+        }
+
+        [TestMethod]
+        public void ClassicGeneticAlgorithm_FitnessLargeThan5()
+        {
+            //Arrange 
+            ClassicGeneticAlgorithm classic = new(new BinaryMutation(0.05f), new RouletteWheelParentSelector(0.05f), new FIFOPopulationSelector(), new SinglePointCrossover(0.05f), 100);
+
+            //Act
+            for (int i = 0; i < 100; i++)
+            {
+                classic.Update();
+                Console.WriteLine($"Iter: {(i + 1):00}\t{classic}");
+            }
+
+            //Assert
+            float fitness = classic.GetAveragePopulationFitness(out var minFitness, out var maxFitness);
+            Assert.IsTrue(fitness > 5);
         }
     }
 }
